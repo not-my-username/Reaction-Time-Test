52cb0447f789dc0d2b1e85ab84e013c35e71d3d0
Liam <something.random.i.made.at.3am@gmail.com>
2022-09-03 20:29:15 +1200

    User can now click the screen

--- index.js
+++ index.js
@@ -20,52 +20,57 @@ function reset() {
     isGreen = false
 }
 
-$(document).ready(function() {
-    $("#mainButton").on("click", function() {
-        switch (state) {
-            case 0:
-                $("body").addClass("bg-red");
-                $("body").removeClass("bg-green");
-                $("#title").text("Wait Until Green")
-                $("#info").text("Click any key when the screen turns green.")
-                $("#mainButton").text("Click Me!")
-                state = 1
-                timeout = setTimeout(turnGreen, Math.floor(Math.random() * (max - min) + min) * 1000)
-                break;
-            case 1:
-                if (isGreen) {
-                    endTime = performance.now()
-                    score[testsRan] = endTime - startTime
-                    $("#title").text(score[testsRan] + "ms")
-                    testsRan++
-                    $("#info").html("Click any key to continue. <b>" + testsRan + " / " + testsToRun + " </b>")
-                    if (testsRan == testsToRun) {
-                        $("#mainButton").text("Finish!")
-                        state = 2
-                    } else {
-                        $("#mainButton").text("Continue!")
-                        reset()
-                    }
-
+function buttonClick() {
+    endTime = performance.now()
+    console.log("Button Click");
+    switch (state) {
+        case 0:
+            $("body").addClass("bg-red");
+            $("body").removeClass("bg-green");
+            $("#title").text("Wait Until Green")
+            $("#info").text("Click any key when the screen turns green.")
+            $("#mainButton").text("Click Me!")
+            state = 1
+            timeout = setTimeout(turnGreen, Math.floor(Math.random() * (max - min) + min) * 1000)
+            break;
+        case 1:
+            if (isGreen) {
+                score[testsRan] = endTime - startTime
+                $("#title").text(score[testsRan] + "ms")
+                testsRan++
+                $("#info").html("Click any key to continue. <b>" + testsRan + " / " + testsToRun + " </b>")
+                if (testsRan == testsToRun) {
+                    $("#mainButton").text("Finish!")
+                    state = 2
                 } else {
-                    clearTimeout(timeout)
-                    $("#title").text("Whoa, Slow Down")
-                    $("#info").text("Wait until the screen turns green before clicking.")
-                    $("#mainButton").text("Try Again!")
+                    $("#mainButton").text("Continue!")
                     reset()
                 }
-                break;
-            case 2:
-                state = 0
-                $("body").removeClass("bg-green")
-                $("#title").text("You Finished! avg: " + Math.round(score.reduce((a, b) => a + b) / score.length) + "ms")
-                $("#info").html("Min: " + Math.min.apply(null, score) + "ms & Max: " + Math.max.apply(null, score) + "ms <br>" + score.join("<br>"))
-                $("#mainButton").text("Again?")
-                break;
-        }
-    });
+
+            } else {
+                clearTimeout(timeout)
+                $("#title").text("Whoa, Slow Down")
+                $("#info").text("Wait until the screen turns green before clicking.")
+                $("#mainButton").text("Try Again!")
+                reset()
+            }
+            break;
+        case 2:
+            state = 0
+            $("body").removeClass("bg-green")
+            $("#title").text("You Finished! avg: " + Math.round(score.reduce((a, b) => a + b) / score.length) + "ms")
+            $("#info").html("Min: " + Math.min.apply(null, score) + "ms & Max: " + Math.max.apply(null, score) + "ms <br>" + score.join("<br>"))
+            $("#mainButton").text("Again?")
+            break;
+    }
+}
+
+$(document).ready(function() {
     $('#timesToRun').on('input', function(e) {
         testsToRun = parseInt($('#timesToRun').val()) || 5
     });
-
+    $("body").on("click", function() {
+        console.log("Screen Click");
+        buttonClick()
+    });
 });
